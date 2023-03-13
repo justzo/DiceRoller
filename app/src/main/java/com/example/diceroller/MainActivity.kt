c92e4a1acbd3b05dd68ed9936b6e1808e40157a4
zohaibmazher <05zm04@gmail.com>
2023-03-13 15:11:47 +0000

    dice logic added - comments

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -15,10 +15,35 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener{
          // val toast = Toast.makeText(this, "dice rolled", Toast.LENGTH_LONG)
           //  toast.show()
-            val textview:TextView = findViewById(R.id.textview)
-            textview.text = "5"
+            // creates pop up with "dice roll" text
 
 
+            //val textview:TextView = findViewById(R.id.textview)
+           // textview.text = "5"
+            // set text view field to 5 when button is pressed
+
+
+            RollDie() // calls specific function
+        }
+
+
+
+
+
+    }
+
+    private fun RollDie() {
+        // 6 sided die created and displayed to user
+        val localDice = Dice(6)
+        val cubeRoll = localDice.roll()
+        val textview:TextView = findViewById(R.id.textview)
+        textview.text = cubeRoll.toString()
+
+    }
+    class Dice (val NumberOfSide: Int){
+        //random number generator above 1
+        fun roll():Int{
+            return (1..NumberOfSide).random()
         }
 
     }
